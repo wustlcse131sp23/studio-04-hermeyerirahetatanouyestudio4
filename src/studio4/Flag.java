b58cb0691227f30392344d5aad45a7d3cfbe69bd
jesusiraheta <jesusiraheta@localhost>
2023-02-23 14:12:06 -0600

    its a slay

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,59 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+	
+		
+	    Color pink = new Color (252,179, 252);
+	    StdDraw.setPenColor(pink);
+	    StdDraw.filledRectangle(.5,.5,1,1);
+		
+		
+		Color lightBlue = new Color(188, 208, 255);
+		StdDraw.setPenColor(lightBlue);
+		StdDraw.filledCircle(.5, .5, .5);
+		
+		Color seafoam = new Color(188, 255, 233);
+		StdDraw.setPenColor(seafoam);
+		StdDraw.filledCircle(.5, .5, .4);
+		
+		Color light = new Color(237, 246, 249);
+		StdDraw.setPenColor(light);
+		StdDraw.filledCircle(.5, .5, .2);
+		
+		
+		Color verde = new Color(6, 81, 67);
+		StdDraw.setPenColor(verde);
+		double [] x= {0.5, 0.6, 0.7, 0.1};
+		double[]y = {0.7, 0.3 , 0.5, 0.7};
+		StdDraw.polygon(x, y);
+		
+		double [] xOne= {0.4, 0.2, 0.8, 0.3};
+		double[]yOne = {0.5, 0.9 , 0.1, 0.2};
+		StdDraw.polygon(xOne, yOne);
+		
+		double [] xTwo= {0.15, 0.3, 0.6, 0.9};
+		double[]yTwo = {0.1, 0.2 , 0.8, 0.7};
+		StdDraw.polygon(xTwo, yTwo);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,59 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		
+		String shape = in.next();
+		System.out.print(shape);
+		
+		int red = in.nextInt();
+		System.out.println(red);
+		
+		
+		int green = in.nextInt();
+		System.out.println(green);
+		
+		int blue = in.nextInt();
+		System.out.println(blue);
+		
+	
+		boolean fill =in.nextBoolean();
+		
+		if( fill == true) {
+			Color RGB = new Color(red,green,blue);
+			StdDraw.setPenColor(RGB);
+			if (shape.equals("rectangle")){
+				double x = in.nextDouble();
+				double y = in.nextDouble();
+				double halfW=in.nextDouble();
+				double halfH=in.nextDouble();
+				StdDraw.filledRectangle(x,y,halfW,halfH);	
+			}
+			
+			
+			
+			
+	
+		}
+		
+		
+		
+		
+		
+		
+		
+		//double [] x= {0.5, 0.6, 0.7};
+	//	double[]y = {0.7, 0.3 , 0.5};
+		//StdDraw.setPenColor(Color.RED);
+	//	StdDraw.polygon(x, y);
+
+		
+		
+		
+		
+		
+		
+		
 	}
 }
+
